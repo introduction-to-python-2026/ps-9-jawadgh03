8814fd833f45b26f202d4d3a5d067afa40ac0e1e
jawadgh03 <jawadghaname12@gmail.com>
2026-01-12 16:30:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,26 +6,25 @@ import joblib
 
 df = pd.read_csv("parkinsons.csv")
 
-features = ["MDVP:Fo(Hz)", "MDVP:RAP"]
+features = ["spread1", "PPE"]
 target = "status"
 
 X = df[features]
 y = df[target]
 
 X_train, X_val, y_train, y_val = train_test_split
-
 (X, y, test_size=0.2, random_state=42)
 
-model = RandomForestClassifier
-(n_estimators=200,
-    max_depth=5,
+model = RandomForestClassifier(
+    n_estimators=300,
+    max_depth=8,
     random_state=42)
 
 model.fit(X_train, y_train)
 
-y_pred = model.predict(X_val)
-print(accuracy_score(y_val, y_pred))
+print(accuracy_score(y_val, model.predict(X_val)))
 
 joblib.dump(model, "parkinsons_model.joblib")
+
 
 
