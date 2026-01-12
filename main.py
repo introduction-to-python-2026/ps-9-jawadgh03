dc2f6a70471cc8113585cf097d8f9a7685c5be4d
jawadgh03 <jawadghaname12@gmail.com>
2026-01-12 15:58:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-import pandas as pd 
-df = pd.read_csv("/content/parkinsons.csv") 
-print(list(df.columns)) 
-selcted_feature = ['MDVP:Flo(Hz)','NHR'] 
-selcted_2 = ['status'] 
-from sklearn.preprocessing import MinMaxScaler
-
-X = df[selcted_feature] 
-y = df[selcted_2]
-scaler = MinMaxScaler()
-X = scaler.fit_transform(X)
-
+import pandas as pd
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
-
-from sklearn.neighbors import KNeighborsClassifier
-
-model = KNeighborsClassifier(n_neighbors=10)
-model.fit(X_train, y_train) 
-
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import joblib
 
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy}') 
+df = pd.read_csv("parkinsons.csv")
 
-selected_features: ["A", "B"]  
-path: "my_model.joblib"
+features = ["MDVP:Fo(Hz)", "MDVP:RAP"]
+target = "status"
+
+X = df[features]
+y = df[target]
+
+scaler = MinMaxScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_val, y_train, y_val = train_test_split
+(X_scaled, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier
+(n_estimators=200,
+    max_depth=5,
+    random_state=42)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_val)
+accuracy = accuracy_score(y_val, y_pred)
+print(accuracy)
+
+joblib.dump(model, "parkinsons_model.joblib")
+
