90630b233a044accc3667e1e8c4cfbb086a1e4c1
jawadgh03 <jawadghaname12@gmail.com>
2026-01-12 16:01:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import joblib
@@ -13,11 +12,9 @@ target = "status"
 X = df[features]
 y = df[target]
 
-scaler = MinMaxScaler()
-X_scaled = scaler.fit_transform(X)
-
 X_train, X_val, y_train, y_val = train_test_split
-(X_scaled, y, test_size=0.2, random_state=42)
+
+(X, y, test_size=0.2, random_state=42)
 
 model = RandomForestClassifier
 (n_estimators=200,
@@ -27,8 +24,8 @@ model = RandomForestClassifier
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_val)
-accuracy = accuracy_score(y_val, y_pred)
-print(accuracy)
+print(accuracy_score(y_val, y_pred))
 
 joblib.dump(model, "parkinsons_model.joblib")
+
 
